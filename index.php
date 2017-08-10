fd37056340ebd407689e6528b3cfd9abd63718bb
innapps <superc20161@gmail.com>
2017-08-09 22:41:13 -0400

    Added necessary files to start using Bootstrap.

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ResponsiveTheme</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
-
 <body>
-Hello! This is just a test.
+    <h1>Hello, world!</h1>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
-
 </html>
