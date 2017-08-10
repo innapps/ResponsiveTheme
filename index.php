00daa9edc77ca72b6b85fa0cc2b1a0272070bb3d
innapps <superc20161@gmail.com>
2017-08-09 23:07:52 -0400

    Styles

--- index.php
+++ index.php
@@ -5,11 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ResponsiveTheme</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory');?>/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory');?>/style.css">
 </head>
 <body>
-    <h1>Hello, world!</h1>
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 bg-info">HEADER</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-9 bg-primary">9</div>
+			<div class="col-sm-3 bg-success">3</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 bg-info">Footer</div>
+		</div>		
+	</div>    
+    
+<script src="<?php echo get_bloginfo('template_directory');?>/js/jquery-3.2.1.min.js"></script>
+<script src="<?php echo get_bloginfo('template_directory');?>/js/bootstrap.min.js"></script>
 </body>
 </html>
